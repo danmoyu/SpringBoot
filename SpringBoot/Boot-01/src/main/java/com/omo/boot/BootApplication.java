0d80ca3adba649fb18cb5ddce3f1778ffaa193d1
Moyu <1067187782@qq.com>
2021-05-02 10:57:50 +0800

    环境搭建

---
+++ SpringBoot/Boot-01/src/main/java/com/omo/boot/BootApplication.java
@@ -0,0 +1,17 @@
+package com.omo.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author MoYu
+ * @create 2021-04-26 10:45
+ */
+@SpringBootApplication
+public class BootApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(BootApplication.class,args);
+    }
+
+}
